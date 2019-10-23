1a39575975673a7d4fc5ebd6f0468e04b255f215
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-23 22:07:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 def my_collect(array)
+<<<<<<< HEAD
   i = 0
   collection = []
   while i < array.length
@@ -8,3 +9,7 @@ def my_collect(array)
   collection
 end
 
+=======
+
+end
+>>>>>>> c2e6b19f3daa18103524d86e3c1f724cdd2b2c6d

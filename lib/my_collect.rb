c38d555ea3e2486f64975aed11ba9fd8641c7dfb
Abraham Moreno <adiaz2821@gmail.com>
2019-11-13 14:58:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(languages)
+  i = 0 
+  collection = [] 
+  while i < languages.length 
+  collection << yield(languages[i])
+  i += 1 
+end 
+collection 
+end 
 
-
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+  language.upcase
+end 
